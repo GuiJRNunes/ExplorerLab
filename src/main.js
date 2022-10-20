7be1c2af06a66e1bb00df8956116c4ace02c03a6
GuiJRNunes <guilherme.j.r.nunes@gmail.com>
2022-10-20 19:48:10 -0300

    adjustments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./css/index.css"
 import IMask from "imask"
 
+// Elementos do cartão
 const card = {
   bgColor01: document.querySelector(".cc-bg svg > g g:nth-child(1) path"),
   bgColor02: document.querySelector(".cc-bg svg > g g:nth-child(2) path"),
@@ -17,6 +18,88 @@ const card = {
   },
 }
 
+const form = {
+  element: document.querySelector("form"),
+  cardNumber: {
+    element: document.querySelector("#card-number"),
+    pattern: {
+      mask: [
+        {
+          mask: "0000 0000 0000 0000",
+          regex: /^4\d{0,15}/,
+          cardtype: "visa",
+        },
+        {
+          mask: "0000 0000 0000 0000",
+          regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+          cardtype: "mastercard",
+        },
+        {
+          mask: "0000 0000 0000 0000",
+          regex: /^1/,
+          cardtype: "elo",
+        },
+        {
+          mask: "0000 0000 0000 0000",
+          cardtype: "default",
+        },
+      ],
+      dispatch: function (appended, dynamicMasked) {
+        const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+        const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+          return number.match(item.regex)
+        })
+
+        return foundMask
+      },
+    },
+    mask: null,
+  },
+  titular: {
+    element: document.querySelector("#card-holder"),
+  },
+  expirationDate: {
+    element: document.querySelector("#expiration-date"),
+    pattern: {
+      mask: "MM{/}YY",
+      blocks: {
+        YY: {
+          mask: IMask.MaskedRange,
+          from: String(new Date().getFullYear()).slice(2),
+          to: String(new Date().getFullYear() + 10).slice(2),
+        },
+        MM: {
+          mask: IMask.MaskedRange,
+          from: 1,
+          to: 12,
+        },
+      },
+    },
+    mask: null,
+  },
+  securityCode: {
+    element: document.querySelector("#security-code"),
+    pattern: {
+      mask: "0000",
+    },
+    mask: null,
+  },
+  addButton: {
+    element: document.querySelector("#add-card"),
+  },
+}
+
+// Aplicar máscaras
+form.cardNumber.mask = IMask(form.cardNumber.element, form.cardNumber.pattern)
+form.expirationDate.mask = IMask(
+  form.expirationDate.element,
+  form.expirationDate.pattern
+)
+form.securityCode.mask = IMask(
+  form.securityCode.element,
+  form.securityCode.pattern
+)
+
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
@@ -25,8 +108,6 @@ function setCardType(type) {
     default: ["black", "gray"],
   }
 
-  console.log(type)
-
   card.bgColor01.setAttribute("fill", colors[type][0])
   card.bgColor02.setAttribute("fill", colors[type][1])
   card.logoImg.setAttribute("src", `cc-${type}.svg`)
@@ -34,90 +115,29 @@ function setCardType(type) {
 
 window.setCardType = setCardType
 
-const securityCode = {
-  element: document.querySelector("#security-code"),
-  pattern: {
-    mask: "0000",
-  },
-  mask: null,
-}
-securityCode.mask = IMask(securityCode.element, securityCode.pattern)
+form.titular.element.addEventListener("input", () => {
+  card.info.titular.innerText = form.titular.element.value || "FULANO DA SILVA"
+})
 
-const expirationDate = {
-  element: document.querySelector("#expiration-date"),
-  pattern: {
-    mask: "MM{/}YY",
-    blocks: {
-      YY: {
-        mask: IMask.MaskedRange,
-        from: String(new Date().getFullYear()).slice(2),
-        to: String(new Date().getFullYear() + 10).slice(2),
-      },
-      MM: {
-        mask: IMask.MaskedRange,
-        from: 1,
-        to: 12,
-      },
-    },
-  },
-  mask: null,
-}
-expirationDate.mask = IMask(expirationDate.element, expirationDate.pattern)
+form.addButton.element.addEventListener("click", () => {
+  alert("Cartão adicionado!")
+})
 
-let foundNumberMask
-const cardNumber = {
-  element: document.querySelector("#card-number"),
-  pattern: {
-    mask: [
-      {
-        mask: "0000 0000 0000 0000",
-        regex: /^4\d{0,15}/,
-        cardtype: "visa",
-      },
-      {
-        mask: "0000 0000 0000 0000",
-        regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
-        cardtype: "mastercard",
-      },
-      {
-        mask: "0000 0000 0000 0000",
-        regex: /^1/,
-        cardtype: "elo",
-      },
-      {
-        mask: "0000 0000 0000 0000",
-        cardtype: "default",
-      },
-    ],
-    dispatch: function (appended, dynamicMasked) {
-      const number = (dynamicMasked.value + appended).replace(/\D/g, "")
-      const foundMask = dynamicMasked.compiledMasks.find(function (item) {
-        return number.match(item.regex)
-      })
-      foundNumberMask = foundMask
+form.element.addEventListener("submit", (event) => {
+  event.preventDefault()
+})
 
-      return foundMask
-    },
-  },
-  mask: null,
-}
-cardNumber.mask = IMask(cardNumber.element, cardNumber.pattern)
+form.securityCode.mask.on("accept", () => {
+  card.extra.securityCode.innerText = form.securityCode.element.value || "123"
+})
 
-const titular = {
-  element: document.querySelector("#card-holder"),
-}
+form.expirationDate.mask.on("accept", () => {
+  card.extra.expirationDate.innerText =
+    form.expirationDate.element.value || "02/32"
+})
 
-function applyChangeCardInfo(input, destElem, defaultValue) {
-  input.addEventListener("change", function () {
-    destElem.innerHTML = this.value || defaultValue
-  })
-}
-
-applyChangeCardInfo(securityCode.element, card.extra.securityCode, "123")
-applyChangeCardInfo(expirationDate.element, card.extra.expirationDate, "02/32")
-applyChangeCardInfo(cardNumber.element, card.info.number, "1234 5678 9012 3456")
-applyChangeCardInfo(titular.element, card.info.titular, "FULANO DA SILVA")
-
-cardNumber.element.addEventListener("change", function () {
-  setCardType(foundNumberMask.cardtype || "default")
+form.cardNumber.mask.on("accept", () => {
+  setCardType(form.cardNumber.mask.masked.currentMask.cardtype)
+  card.info.number.innerText =
+    form.cardNumber.element.value || "1234 5678 9012 3456"
 })
